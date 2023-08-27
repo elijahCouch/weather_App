5ecf2dbc1eef14d448cc0db20d8a0b2a8bdc48cd
elijahCouch <143055348+elijahCouch@users.noreply.github.com>
2023-08-26 18:27:45 -0600

    Update forecast.js

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -26,7 +26,7 @@ const Forecast = ({ data }) => {
                   <img src={`icons/${item.weather[0].icon}.png`} className="icon-small" alt="weather" />
                   <label className="day">{forecastDays[idx]}</label>
                   <label className="description">{item.weather[0].description}</label>
-                  <label className="min-max">{Math.round(item.main.temp_max)}°C /{Math.round(item.main.temp_min)}°C</label>
+                  <label className="min-max">{Math.round(item.main.temp_max)}°C</label>
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
